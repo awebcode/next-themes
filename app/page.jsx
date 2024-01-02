7d01dcf4236de3e9ce245cee071b9444242d3cf9
Md-Asikur <asikurrahaman997@gamil.com>
2024-01-02 14:00:04 +0600

    updated

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,9 @@ export default function Home() {
   return (
     <section className='py-24'>
       <div className='container'>
-        <h1 className='text-3xl font-bold'>NextJs 13 Starter with Theme</h1>
+        <h1 className='text-4xl md:text-9xl font-extrabold'>
+          NextJs 13 Starter with Theme
+        </h1>
       </div>
     </section>
   )
